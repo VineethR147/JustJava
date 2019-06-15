dfd2aebc05bbb2d50cdfc21adae22d06d2178d01
VineethR147 <vineeth.181ee254@nitk.edu.in>
2019-06-15 08:20:46 +0530

    Second Commit

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -25,37 +25,41 @@ public class MainActivity extends AppCompatActivity {
     public void increment(View view)
     {
     quantity=quantity + 1;
-    display(quantity);
+    displayQuantity(quantity);
     }
     public void decrement(View view)
     {
     quantity=quantity-1;
-        display(quantity);
+        displayQuantity(quantity);
     }
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-       int price = quantity * 5 ;
-       String priceMessage = "Total: " + price;
-       priceMessage = priceMessage + "\n Thankyou";
-       displayMessage(priceMessage);
+
+      int price = calculatePrice();
+       displayMessage(createOrderSummary( price));
+
+    }
+
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+    private String createOrderSummary(int price){
+        String priceMessage = "Total  :$ " + price;
+        priceMessage ="Name Vineeth \n"+ priceMessage + "\nQuantity : "+ quantity +"\nThank you" ;
+       return priceMessage;
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
-     * This displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+   
     /**
      * This method displays the given text on the screen.
      */
@@ -63,5 +67,6 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
+
 
 }
